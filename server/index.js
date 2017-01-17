f79bdb8e6cc94c262dad4f4d7c6bccad33d78a3f
Snær Seljan Þóroddsson <snaer.seljan.thoroddsson@islandsbanki.is>
2017-01-17 14:10:41 +0000

    Get trailers

--- server/index.js
+++ server/index.js
@@ -23,6 +23,8 @@ mongoose.connect(dbUrl, (error) => {
     }
 });
 
+// Only if no access to database
+// Remember to remove
 movieSync((error) => {
     if (error) {
         console.log('Error', error);
--- server/services/moviesync.js
+++ server/services/moviesync.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 const _ = require('lodash');
-const apiKey = process.env.API_KEY_KVIKMYNDIR;
+const apiKeyKvikmyndir = process.env.API_KEY_KVIKMYNDIR;
+const apiKeyTmdb = process.env.API_KEY_TMDB;
 
 module.exports = (callback) => {
     let allMovies = []; // Contains all movies for 5 days in one big array [movie1, movie2, movie2, ...]
@@ -25,8 +26,7 @@ module.exports = (callback) => {
             upcomingMovies.data = data
             mergedList = _.unionBy(mergedList, data, 'id');
 
-            // Get plot for each movie in array
-            return getPlotForMovies(mergedList);
+            return getPlotForMovies(mergedList); // Get plot for each movie in array
         })
         .then(plots => {
             // Add plot to showtime movies and upcoming movies
@@ -35,7 +35,60 @@ module.exports = (callback) => {
             });
 
             upcomingMovies.data = addPlotToMovies(upcomingMovies.data, plots);
+            const trailers = getTrailers(allMovies); // BÆTA VIÐ UPCOMING
+            const test = '';
         }).catch(error => console.error(error));
+}
+
+function getTrailers(movies) {
+    return new Promise((resolve, reject) => {
+        let trailersArr = []; // Contains all trailers object
+        let promises = [];
+
+        _.forEach(movies, (movie) => {
+            if (movie.ids && movie.ids.imdb) {
+                const imdbId = movie.ids.imdb.indexOf('tt') > -1 ? movie.ids.imdb : `tt${movie.ids.imdb}`;
+                const url = `https://api.themoviedb.org/3/movie/${imdbId}/videos?api_key=${apiKeyTmdb}`;
+
+                const request = fetch(url)
+                    .then(res => res.json())
+                    .then(trailers => {
+                        if (trailers.results && trailers.results.length > 0) {
+                            let trailersObj = {
+                                imdb: movie.ids.imdb,
+                                data: []
+                            }
+
+                            _.forEach(trailers, (trailer) => {
+                                trailersObj.data.push({
+                                    id: trailer.id,
+                                    url: `https://www.youtube.com/embed/${trailer.key}&rel=0`,
+                                    size: trailer.size,
+                                    name: trailer.name
+                                });
+                            });
+
+                            trailersArr.push(trailersObj);
+                        }
+                    }).catch(error => {return console.log(error);reject(error)});
+
+                promises.push(request);
+            }
+        });
+
+        Promise.all(promises)
+            .then(() => resolve(trailersArr))
+            .catch(error => reject(error));
+    });
+
+    //config.themoviedburl + 'tt' + imdbid + '/videos?api_key='
+    //   var trailers = JSON.parse(data);
+    //     if(trailers.results && trailers.results.length > 0) {
+    //         for(var t = 0; t < trailers.results.length; t++) {
+    //             trailers.results[t].url = 'https://www.youtube.com/embed/' + trailers.results[t].key + '?rel=0';
+    //         }
+    //     }
+    //     film.trailers.push(trailers);
 }
 
 // Makes get request to Kvikmyndir.is API to get movie showtimes
@@ -46,7 +99,7 @@ function getKvikmyndir() {
         let promises = [];
 
         for (let i = 0; i < 5; i++) {
-            const url = `http://kvikmyndir.is/api/showtimes_by_date/?key=${apiKey}&dagur=${i}`;
+            const url = `http://kvikmyndir.is/api/showtimes_by_date/?key=${apiKeyKvikmyndir}&dagur=${i}`;
 
             const request = fetch(url)
                 .then(res => res.json())
@@ -72,7 +125,7 @@ function getKvikmyndir() {
 // @returns {Promise} Promise - the promise object
 function getUpcoming() {
     return new Promise((resolve, reject) => {
-        const url = `http://kvikmyndir.is/api/movie_list_upcoming/?key=${apiKey}&count=100`;
+        const url = `http://kvikmyndir.is/api/movie_list_upcoming/?key=${apiKeyKvikmyndir}&count=100`;
 
         fetch(url)
             .then(res => res.json())
@@ -92,15 +145,14 @@ function getPlotForMovies(movies) {
 
         for (let i = 0; i < movies.length; i++) {
             const imdbId = movies[i].ids.imdb;
-            const url = `http://kvikmyndir.is/api/movie/?imdb=${imdbId}&key=${apiKey}`;
+            const url = `http://kvikmyndir.is/api/movie/?imdb=${imdbId}&key=${apiKeyKvikmyndir}`;
             const request = fetch(url)
                 .then(res => res.json())
                 .then(data => {
                     if (data.plot && data.imdb) {
                         moviesWithPlot.push({
                             imdb: data.imdb,
-                            text: data.plot,
-                            title: data.title
+                            text: data.plot
                         });
                     }
                 })
