50982976ce01dbf2c1ef6c3fa7da84e6a23197a0
Snær Seljan Þóroddsson <snaer.seljan.thoroddsson@islandsbanki.is>
2017-01-19 14:55:23 +0000

    Omdb requests

--- server/services/moviesync.js
+++ server/services/moviesync.js
@@ -15,6 +15,9 @@ module.exports = (callback) => {
         type: 'upcoming',
         data: []
     }; // Contains upcoming movies { ... ,data: [movie1, movie2, movie2, ...] }
+    let plotsArr = []; // Array of movie plots object [{imdb:'tt0317248',text: 'This is movie plot text'}, ...]
+    let trailersArr = [] // Array of trailer information for movie [{imdb:'tt0317248',data: [{...}, {...}, ...]]
+    let omdbArr = [] // Array of omdb information for movie [{imdb:'tt0317248',data: {...}, ...]
 
     getKvikmyndir()
         .then(data => {
@@ -30,93 +33,22 @@ module.exports = (callback) => {
 
             return getPlotForMovies(mergedList); // Get plot for each movie in array
         }).then(plots => {
+            plotsArr = plots;
+
             // Add plot to showtime movies and upcoming movies
             _.forEach(moviesByDay, (day, key) => {
-                day.data = addPlotToMovies(day.data, plots);
+                day.data = addPlotToMovies(day.data, plotsArr);
             });
 
-            upcomingMovies.data = addPlotToMovies(upcomingMovies.data, plots);
+            upcomingMovies.data = addPlotToMovies(upcomingMovies.data, plotsArr);
 
-            return getTrailers(mergedList);
-        }).then(trailers => {
-            console.log(trailers);
+            return getTrailers(mergedList); // Get trailers for each movie in array
+        }).then(trailersData => {
+            trailersArr = trailersData;
+            return getOmdbData(mergedList); // Get omdb information for each movie in array
+        }).then(omdbData => {
+            omdbArr = omdbData;
         }).catch(error => console.error(error));
-}
-
-/**
- * Gets trailers for multiple movies from TMDB API
- * @param {Array} movies - Array of movie objects
- * @returns {Promise} promise - When all promises have resolved then trailersArr is returned
- */
-function getTrailers(movies) {
-    return new Promise((resolve, reject) => {
-        let trailersArr = []; // Contains all trailers object
-        let promises = [];
-
-        for (let i = 0; i < movies.length; i++) {
-            const movie = movies[i];
-
-            if (movie.ids && movie.ids.imdb) {
-                const imdbId = movie.ids.imdb.indexOf('tt') > -1 ? movie.ids.imdb : `tt${movie.ids.imdb}`;
-                const url = `https://api.themoviedb.org/3/movie/${imdbId}/videos?api_key=${apiKeyTmdb}`;
-                const delay = 400 * i; // TMDB has 30 request per 10 seconds
-
-                const request = getTrailersRequest(url, imdbId, delay)
-                    .then(trailer => {
-                        if (trailer) {
-                            trailersArr.push(trailer);
-                        }
-                    })
-                    .catch(error => reject(error));
-                promises.push(request);
-            }
-        }
-
-        Promise.all(promises)
-            .then(() => resolve(trailersArr))
-            .catch(error => reject(error));
-    });
-}
-
-function getTrailersRequest(url, imdbId, delay) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            fetch(url)
-                .then(res => res.json())
-                .then(trailers => {
-                    if (!trailers && !trailers.results) {
-                        return resolve();
-                    } 
-                    
-                    const trailersObj = createTrailerObject(imdbId, trailers.results);
-                    return resolve(trailersObj);
-                }).catch(error => reject(error));
-        }, delay);
-    });
-}
-
-/**
- * Creates new trailer object from TMDB trailer object
- * @param {String} imdbId - Imdb id
- * @param {Array} trailers - Array of trailer objecs
- * @returns {Object} trailersObj - newly created trailer object
- */
-function createTrailerObject(imdbId, trailers) {
-    let trailersObj = {
-        imdb: imdbId,
-        data: []
-    };
-
-    _.forEach(trailers, (trailer) => {
-        trailersObj.data.push({
-            id: trailer.id,
-            url: `https://www.youtube.com/embed/${trailer.key}?rel=0`,
-            size: trailer.size,
-            name: trailer.name
-        });
-    });
-
-    return trailersObj;
 }
 
 /**
@@ -222,6 +154,126 @@ function addPlotToMovies(movies, plots) {
 }
 
 /**
+ * Gets trailers for multiple movies from TMDB API
+ * @param {Array} movies - Array of movie objects
+ * @returns {Promise} promise - When all promises have resolved then trailersArr is returned
+ */
+function getTrailers(movies) {
+    return new Promise((resolve, reject) => {
+        let trailersArr = []; // Contains all trailers object
+        let promises = [];
+
+        for (let i = 0; i < movies.length; i++) {
+            const movie = movies[i];
+
+            if (movie.ids && movie.ids.imdb) {
+                const imdbId = formatImdbId(movie.ids.imdb);
+                const url = `https://api.themoviedb.org/3/movie/${imdbId}/videos?api_key=${apiKeyTmdb}`;
+                const delay = 400 * i; // TMDB has 30 request per 10 seconds
+
+                const request = getTrailersRequest(url, imdbId, delay)
+                    .then(trailer => {
+                        if (trailer) {
+                            trailersArr.push(trailer);
+                        }
+                    })
+                    .catch(error => reject(error));
+                promises.push(request);
+            }
+        }
+
+        Promise.all(promises)
+            .then(() => resolve(trailersArr))
+            .catch(error => reject(error));
+    });
+}
+
+/**
+ * Gets trailers from TMDB service by specific imdbID
+ * @param {String} url - url
+ * @param {String} imdbId - IMDB id
+ * @param {Int} delay - Delay in milliseconds
+ * @returns {Promise} promise - Promise witch returns trailer object
+ */
+function getTrailersRequest(url, imdbId, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            fetch(url)
+                .then(res => res.json())
+                .then(trailers => {
+                    if (!trailers && !trailers.results) {
+                        return resolve();
+                    }
+
+                    const trailersObj = createTrailerObject(imdbId, trailers.results);
+                    return resolve(trailersObj);
+                }).catch(error => reject(error));
+        }, delay);
+    });
+}
+
+/**
+ * Creates new trailer object from TMDB trailer object
+ * @param {String} imdbId - Imdb id
+ * @param {Array} trailers - Array of trailer objecs
+ * @returns {Object} trailersObj - newly created trailer object
+ */
+function createTrailerObject(imdbId, trailers) {
+    let trailersObj = {
+        imdb: imdbId,
+        data: []
+    };
+
+    _.forEach(trailers, (trailer) => {
+        trailersObj.data.push({
+            id: trailer.id,
+            url: `https://www.youtube.com/embed/${trailer.key}?rel=0`,
+            size: trailer.size,
+            name: trailer.name
+        });
+    });
+
+    return trailersObj;
+}
+
+/**
+ * Iterates movies objects and make request to OMDB API for each movie
+ * Creates a omdb array to store the omdb API data
+ * @param {Array} movies - Array of movie objects
+ * @returns {Promise} promise - When all promises have resolved then omdbArr is returned
+ */
+function getOmdbData(movies) {
+    return new Promise((resolve, reject) => {
+        let omdbArr = []; // Contains all trailers object
+        let promises = [];
+
+        for (let i = 0; i < movies.length; i++) {
+            const movie = movies[i];
+
+            if (movie.ids && movie.ids.imdb) {
+                const imdbId = formatImdbId(movie.ids.imdb);
+                const url = `http://www.omdbapi.com/?i=${imdbId}&plot=true&tomatoes=true&r=json`;
+
+                const request = fetch(url)
+                    .then(res => res.json())
+                    .then(data => {
+                        omdbArr.push({
+                            imdb: data.imdbID,
+                            data: data
+                        });
+                    }).catch(error => reject(error));
+
+                promises.push(request);
+            }
+        }
+
+        Promise.all(promises)
+            .then(() => resolve(omdbArr))
+            .catch(error => reject(error));
+    });
+}
+
+/**
  * Iterates array of arrays, and puts those object into new array
  * @param {Array} array
  * @returns {Array} newArray
@@ -237,4 +289,13 @@ function mergeMovieArrays(array) {
     }
 
     return newArray;
+}
+
+/**
+ * Adds tt before imdb id if not exist
+ * @param {String} imdbId - IMDB id
+ * @returns {String} imdb id formatted
+ */
+function formatImdbId(imdbId) {
+    return imdbId.indexOf('tt') > -1 ? imdbId : `tt${imdbId}`;
 }
